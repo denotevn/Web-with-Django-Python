59c8426c6e52c6585f4901dd728c225ec57cd651
Tuan Dinh <dinhngoctuan2131997@gmail.com>
2022-08-27 13:59:14 +0300

    POST and GET

--- myfirstproject/myapp/views.py
+++ myfirstproject/myapp/views.py
@@ -16,7 +16,7 @@ def index(request):
     return render(request,'index.html', context)
 
 def counter(request):
-    text = request.GET['text'] # text is nametype of <form .... in index.html
+    text = request.POST['text'] # text is nametype of <form .... in index.html
     amount_of_word = len(text.split())
     ## Sau khi tinh xong thi can gui thang "amount_of_words" vao file "counter.html"
     return render(request, 'counter.html', {'amount':amount_of_word})
