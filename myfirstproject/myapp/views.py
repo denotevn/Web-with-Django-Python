a1c1d8eb71024088b956cbde4391280e6a60c485
Tuan Dinh <dinhngoctuan2131997@gmail.com>
2022-08-27 13:40:38 +0300

    counter in web

--- myfirstproject/myapp/views.py
+++ myfirstproject/myapp/views.py
@@ -14,3 +14,9 @@ def index(request):
     }
 
     return render(request,'index.html', context)
+
+def counter(request):
+    text = request.GET['text'] # text is nametype of <form .... in index.html
+    amount_of_word = len(text.split())
+    ## Sau khi tinh xong thi can gui thang "amount_of_words" vao file "counter.html"
+    return render(request, 'counter.html', {'amount':amount_of_word})
