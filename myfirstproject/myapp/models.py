43b25d6b414b813ed084d6b58abd11c396376494
Tuan Dinh <dinhngoctuan2131997@gmail.com>
2022-08-28 11:31:22 +0300

    manage data with admin

--- myfirstproject/myapp/models.py
+++ myfirstproject/myapp/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    id: int
-    name: str
-    details: str
-    is_true: bool
-    
+class Feature(models.Model):
+    # id: int ko can vi da tu dong load phan nay ket noi voi uan ly database
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
+     
